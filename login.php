8a86822e2350dcbcf6d0d84870149f9e4f468c66
kurtjohn21 <kjmojado21@gmail.com>
2024-04-11 15:11:13 +0800

    added login ui

--- login.php
+++ login.php
@@ -7,6 +7,16 @@
 </head>
 <body>
     <!-- create login page -->
+    <div class="card mx-auto">
+        <div class="card-header">
+            <h1 class="text-center">Login</h1>
+        </div>
+        <div class="card-body">
+            <form action="login.php" method="post">
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Email address</label>
+                    <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"
+    </div>
         
 
 </body>
